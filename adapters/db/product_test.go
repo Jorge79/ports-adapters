a81fb7f94691e6d4ba52bb1e1f1d41d2899b70c3
Jorge79 <Jorge7.9@hotmail.com>
2024-01-18 17:56:17 -0300

    test Save method

--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/Jorge79/ports-adapters/adapters/db"
+	"github.com/Jorge79/ports-adapters/application"
 	"github.com/stretchr/testify/require"
 )
 
@@ -51,4 +52,28 @@ func TestProductDB_Get(t *testing.T) {
 	require.Equal(t, "Product test", product.GetName())
 	require.Equal(t, 0.0, product.GetPrice())
 	require.Equal(t, "disabled", product.GetStatus())
+}
+
+func TestProductDB_Save(t *testing.T) {
+	setup()
+	defer Db.Close()
+	productDb := db.NewProductDb(Db)
+
+	product := application.NewProduct()
+	product.Name = "Product Test"
+	product.Price = 25
+
+	productResult, err := productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+
+	product.Status = "enabled"
+
+	productResult, err = productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
 }
